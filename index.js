cb56ffac89b2cda07299344ca6d93139bf6a49ea
jrburke <jrburke@gmail.com>
2013-01-05 16:46:26 -0800

    rev for 0.0.2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
     "keywords": [
         "volo"
     ],
-    "version": "0.0.1",
+    "version": "0.0.2",
     "homepage": "http://github.com/volojs/volo-appcache",
     "author": "James Burke <jrburke@gmail.com> (http://github.com/jrburke)",
     "licenses": [
